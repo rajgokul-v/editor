5235ca9ce347ae8ce7c2d5dd9183e5aab67676aa
Gokulraj V <rakgokul.v2000@gmail.com>
2021-03-27 23:17:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import SyntaxHighlighterComponent from './SyntaxHighlighterComponent'
 import './app.css'
+import gfm from 'remark-gfm'
 
 function App() {
   const [editorInput, setEditorInput] = useState('')
@@ -19,6 +20,7 @@ function App() {
       <div className="preview">
         <ReactMarkdown
           source={editorInput}
+          plugins={[gfm]}
           renderers={{ code: SyntaxHighlighterComponent }}
         />
       </div>
